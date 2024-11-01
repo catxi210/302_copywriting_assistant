c3a7f22ac4cdd7b103f31192403faf96004cdb12
yangxinzhang <2560975990@qq.com>
2024-11-01 16:14:25 +0800

    Replace invalid code

--- src/components/PoweredBy.tsx
+++ src/components/PoweredBy.tsx
@@ -1,10 +1,10 @@
 "use client";
-import { selectGlobal } from "@/app/store/globalSlice"
-import { useAppSelector } from "@/app/store/hooks"
 import { LANGUAGE_LIBRARY } from "@/constant/language";
 
 export default function PoweredBy({ language }: { language: 'chinese' | 'english' | 'japanese' }) {
-    const global = useAppSelector(selectGlobal)
+    const href = process.env.NEXT_PUBLIC_REGION ?
+    process.env.NEXT_PUBLIC_OFFICIAL_WEBSITE_URL_GLOBAL :
+    process.env.NEXT_PUBLIC_OFFICIAL_WEBSITE_URL_CHINA;
 
     return <div
         className="flex justify-center items-center flex-col my-3 absolute bottom-0 left-1/2 -translate-x-1/2"
@@ -15,7 +15,7 @@ export default function PoweredBy({ language }: { language: 'chinese' | 'english
         </div>
         <div className="flex justify-center items-center gap-1">
             Powered By
-            <a target="_blank" href={global.region ? "https://302.ai/" : "https://302ai.cn/"}><img className="object-contain" src="https://file.302.ai/gpt/imgs/91f7b86c2cf921f61e8cf9dc7b1ec5ee.png" alt="gpt302" width="55" /></a>
+            <a target="_blank" href={href}><img className="object-contain" src="https://file.302.ai/gpt/imgs/91f7b86c2cf921f61e8cf9dc7b1ec5ee.png" alt="gpt302" width="55" /></a>
         </div>
     </div >
 }
